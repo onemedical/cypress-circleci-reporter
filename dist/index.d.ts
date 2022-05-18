33c3dcc19d1ee0bcdf304b4ed3c3b00183d2264e
Natasha Hull-Richter <nhullrichter@onemedical.com>
2022-05-18 09:30:50 -0700

    Revert "Actually commit the dist folder"
    This reverts commit 0ad6b50c699cf84cba4784d64b25d785119c97bb.

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import Mocha, { Runner, MochaOptions } from 'mocha';
-declare class CypressCircleCIReporter extends Mocha.reporters.Base {
-    file: string;
-    constructor(runner: Runner, options?: MochaOptions);
-    private getTestcaseAttributes;
-}
-export default CypressCircleCIReporter;
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./cypress-circleci-reporter.cjs.production.min.js')
-} else {
-  module.exports = require('./cypress-circleci-reporter.cjs.development.js')
-}
